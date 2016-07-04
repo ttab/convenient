0531ac9f9deb2381d27b57767561a90d738b2cf8
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2016-07-04 13:16:08 +0200

    invoke -> ninvoke

--- test/ninvoke-test.js
+++ test/ninvoke-test.js
@@ -1,6 +1,8 @@
-var invoke = require('../lib/invoke');
+/*global describe,it,stub,beforeEach,expect */
 
-describe('invoke', function () {
+var ninvoke = require('../lib/ninvoke');
+
+describe('ninvoke', function () {
     var obj;
     beforeEach(function () {
         obj = {
@@ -10,16 +12,24 @@ describe('invoke', function () {
 
     it('returns a fulfilled promise on success', function () {
         obj.method.callsArgWith(2, undefined, 'hello', 'world');
-        return invoke(obj, 'method', 1, 2).then(function (result) {
+        return ninvoke(obj, 'method', 1, 2).then(function (result) {
             obj.method.should.have.been.calledOnce;
             obj.method.should.have.been.calledWith(1, 2);
             result.should.equal('hello');
         });
     });
 
+    it('binds this', function (done) {
+        obj.method = function () {
+            expect(this).to.equal(obj);
+            done();
+        };
+        ninvoke(obj, 'method', 1, 2);
+    });
+
     it('returns a rejected promise on failure', function () {
         obj.method.callsArgWith(2, 'panda attack!');
-        return invoke(obj, 'method', 1, 2)
+        return ninvoke(obj, 'method', 1, 2)
             .should.eventually.be.rejectedWith('panda attack!')
             .and.then(function () {
                 obj.method.should.have.been.calledOnce;
@@ -28,7 +38,7 @@ describe('invoke', function () {
 
     it('returns a rejected promise on exceptions', function () {
         obj.method.throws(new Error('panda attack!'));
-        return invoke(obj, 'method', 1, 2)
+        return ninvoke(obj, 'method', 1, 2)
             .should.eventually.be.rejectedWith('panda attack!')
             .and.then(function () {
                 obj.method.should.have.been.calledOnce;
